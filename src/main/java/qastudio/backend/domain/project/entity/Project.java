ef0c5fbd026d7081867ff5a54bd82d47d9390a69
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-01-08 21:48:41 +0900

    🔨 Refactor: project column 어노테이션 추가

--- src/main/java/qastudio/backend/domain/project/entity/Project.java
+++ src/main/java/qastudio/backend/domain/project/entity/Project.java
@@ -17,11 +17,13 @@ public class Project extends BaseEntity {
     @Column(name = "project_id")
     private Long id;
 
+    @Column(name = "project_image")
     private String projectImage;
 
     @Column(nullable = false)
     private String projectName;
 
+    @Column(name = "project_url")
     private String projectUrl;
 
     @Column(length = 500)
