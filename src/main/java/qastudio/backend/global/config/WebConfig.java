6ab5d1fdfda719630a7b5f5976cc0e021ec40da2
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-01-11 23:45:20 +0900

    ✨ Feature: Cors 설정

--- src/main/java/qastudio/backend/global/config/WebConfig.java
+++ src/main/java/qastudio/backend/global/config/WebConfig.java
@@ -1,10 +1,15 @@
 package qastudio.backend.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import qastudio.backend.global.handler.resolver.AuthResolver;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -14,6 +19,27 @@ public class WebConfig implements WebMvcConfigurer {
 
     public WebConfig(AuthResolver authResolver) {
         this.authResolver = authResolver;
+    }
+
+    @Bean
+    public static CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        List<String> allowedOriginPatterns = Arrays.asList("*");
+        configuration.setAllowedOriginPatterns(allowedOriginPatterns);
+
+        List<String> allowedHttpMethods = Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH");
+        configuration.setAllowedMethods(allowedHttpMethods);
+
+        List<String> allowedHeaders = Arrays.asList("*");
+        configuration.setAllowedHeaders(allowedHeaders);
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Override
