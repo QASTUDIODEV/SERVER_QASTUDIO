fefefe0928eb6f88d474e211e7f42c3e51b4b8fc
7beunseo <kimes0403@gmail.com>
2025-01-13 00:17:05 +0900

    🔨 Refactor: 프로젝트 초대 시 RequestBody에 projectId, userId 추가

--- src/main/java/qastudio/backend/domain/project/dto/request/TeamMemberRequest.java
+++ src/main/java/qastudio/backend/domain/project/dto/request/TeamMemberRequest.java
@@ -1,9 +1,11 @@
 package qastudio.backend.domain.project.dto.request;
 
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.util.List;
@@ -11,11 +13,24 @@ import java.util.List;
 public class TeamMemberRequest {
 
     @Getter
-    public static class EmailList {
+    public static class Invite {
+        // project pk
+        @NotNull(message = "projectId는 필수 입력 값입니다.")
+        private Long projectId;
         // 이메일 리스트
         @NotEmpty(message = "emailList 는 필수 입력 값입니다.")
-        private List<@Email(message = "이메일 형식에 맞지 않습니다.")
-                     @NotBlank(message = "이메일은 필수 입력 값입니다.") String> emailList;
+        private List<@Valid MemberEmail> memberEmailList;
+    }
+
+    @Getter
+    public static class MemberEmail {
+        // 유저 pk
+        @NotNull(message = "userId 는 필수 입력 값입니다.")
+        private Long userId;
+        // 이메일
+        @Email(message = "이메일 형식에 맞지 않습니다.")
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        private String email;
     }
 
 }
