25fb1e06de597e880de2e46c05885fa7928a4bb7
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2025-01-17 23:07:57 +0900

    refactor : json 변환 공통모듈로

--- src/main/java/qastudio/backend/domain/scenario/converter/ActionConverter.java
+++ src/main/java/qastudio/backend/domain/scenario/converter/ActionConverter.java
@@ -1,35 +1,26 @@
 package qastudio.backend.domain.scenario.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import qastudio.backend.domain.scenario.dto.Element;
 import qastudio.backend.domain.scenario.dto.request.ScenarioRequest;
 import qastudio.backend.domain.scenario.entity.ActionTable;
 import qastudio.backend.domain.scenario.entity.Scenario;
+import qastudio.backend.global.util.JsonUtil;
 
 import java.util.List;
-
 @Component
 public class ActionConverter {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     public ActionTable toEntity(ScenarioRequest.CreateScenarioRequest.Action action, Long scenarioId) {
-        String actionElements = convertToJson(action.getElements());
-
         return ActionTable.builder()
                 .actionName(action.getActionName())
                 .step(action.getStep())
                 .scenario(Scenario.builder().id(scenarioId).build())
-                .actionElements(actionElements)
+                .actionElements(JsonUtil.toJson(action.getElements()))
                 .build();
     }
-    private String convertToJson(List<Element> elements) {
-        try {
-            return objectMapper.writeValueAsString(elements);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("JSON 변환 실패: " + e.getMessage());
-        }
-    }
+
 }
