8350775fd170771d81c44f1b266d063cb928c83a
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-01-20 17:01:51 +0900

    🐞 Fix: Role 삭제

--- src/main/java/qastudio/backend/domain/user/entity/User.java
+++ src/main/java/qastudio/backend/domain/user/entity/User.java
@@ -2,14 +2,10 @@ package qastudio.backend.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import qastudio.backend.domain.user.entity.enums.Role;
 import qastudio.backend.domain.project.entity.UserProject;
 import qastudio.backend.global.comon.domain.BaseEntity;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -17,7 +13,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class User extends BaseEntity implements UserDetails {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,50 +29,12 @@ public class User extends BaseEntity implements UserDetails {
     @Column(name = "banner_image")
     private String bannerImage;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<AccountTable> accounts = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserProject> userProjects = new ArrayList<>();
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(role);
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
     public void addAccount(AccountTable accountTable) {
         this.accounts.add(accountTable);
--- src/main/java/qastudio/backend/global/oauth/CustomOAuth2UserService.java
+++ src/main/java/qastudio/backend/global/oauth/CustomOAuth2UserService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import qastudio.backend.domain.user.entity.AccountTable;
 import qastudio.backend.domain.user.entity.User;
-import qastudio.backend.domain.user.entity.enums.Role;
 import qastudio.backend.domain.user.repository.AccountTable.AccountTableRepository;
 import qastudio.backend.global.apiPayload.code.exception.custom.AuthException;
 import qastudio.backend.global.apiPayload.code.status.ErrorStatus;
@@ -53,7 +52,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         User user = getOrSave(oAuth2UserInfo, email);
 
         return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority(user.getRole().name())),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
                 updatedAttributes,
                 userNameAttributeName
         );
@@ -68,7 +67,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private User createUser(OAuth2UserInfo oAuth2UserInfo, String email) {
         User user = User.builder()
                 .nickname("")
-                .role(Role.USER)
                 .build();
 
         AccountTable accountTable = AccountTable.builder()
