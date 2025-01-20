0502cd5758a80b566610f229a7d0e37d3e619fe9
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-01-20 16:31:47 +0900

    ⚙ Setting: Redis 추가

--- src/main/java/qastudio/backend/domain/auth/dto/request/AuthRequest.java
+++ src/main/java/qastudio/backend/domain/auth/dto/request/AuthRequest.java
@@ -1,9 +1,7 @@
 package qastudio.backend.domain.auth.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
---
+++ src/main/java/qastudio/backend/global/config/RedisConfig.java
@@ -0,0 +1,7 @@
+package qastudio.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+}
