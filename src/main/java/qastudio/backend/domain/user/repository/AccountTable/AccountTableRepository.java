6ed3e73455189b6b75148140ca6ea1145507318e
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-01-23 23:37:11 +0900

    🔨 Refactor: 불필요한 import 삭제

--- src/main/java/qastudio/backend/domain/user/repository/AccountTable/AccountTableRepository.java
+++ src/main/java/qastudio/backend/domain/user/repository/AccountTable/AccountTableRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import qastudio.backend.domain.user.entity.AccountTable;
 import qastudio.backend.domain.user.entity.enums.EmailType;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface AccountTableRepository extends JpaRepository<AccountTable, Long>, AccountTableRepositoryCustom {
-    Optional<AccountTable> findByEmailAndEmailType(String email, EmailType emailType);}
+    Optional<AccountTable> findByEmailAndEmailType(String email, EmailType emailType);
+}
+
