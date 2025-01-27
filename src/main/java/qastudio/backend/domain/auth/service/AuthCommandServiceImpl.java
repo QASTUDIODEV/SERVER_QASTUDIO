22993ac746c5e42e05239eba314ceef4a94f429a
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-01-27 11:16:41 +0900

    🐛 Fix: 일반 로그인 리다이렉트 없애기

--- src/main/java/qastudio/backend/domain/auth/service/AuthCommandServiceImpl.java
+++ src/main/java/qastudio/backend/domain/auth/service/AuthCommandServiceImpl.java
@@ -91,27 +91,18 @@ public class AuthCommandServiceImpl implements AuthCommandService {
 
 
     @Override
-    public AuthResponse.LoginResponse localLogin(AuthRequest.LocalRequest loginRequest, HttpServletResponse response) {
+    public void localLogin(AuthRequest.LocalRequest loginRequest, HttpServletResponse response) {
         try {
             TokenInfo tokenInfo = authenticateAndGenerateToken(loginRequest.getEmail(), loginRequest.getPassword());
 
-//            Cookie existing_user_cookie = authConverter.createCookie("existing_user", "true", 1800);
-//            Cookie accessToken_cookie = authConverter.createCookie("accessToken", loginResponse.getToken().getAccessToken(), 1800);
-//            Cookie refreshToken_cookie = authConverter.createCookie("refreshToken", loginResponse.getToken().getRefreshToken(), 604800);
-//
-//            response.addCookie(existing_user_cookie);
-//            response.addCookie(accessToken_cookie);
-//            response.addCookie(refreshToken_cookie);
-//
-//            response.sendRedirect(REDIRECT_URL);
+            Cookie existing_user_cookie = authConverter.createCookie("existing_user", "true", 1800);
+            Cookie accessToken_cookie = authConverter.createCookie("accessToken", tokenInfo.getAccessToken(), 1800);
+            Cookie refreshToken_cookie = authConverter.createCookie("refreshToken", tokenInfo.getRefreshToken(), 604800);
 
-            AuthResponse.LoginResponse loginResponse = authConverter.toLoginResponse(tokenInfo, true);
+            response.addCookie(existing_user_cookie);
+            response.addCookie(accessToken_cookie);
+            response.addCookie(refreshToken_cookie);
 
-            return loginResponse;
-
-//        } catch (IOException e) {
-//            log.error("Redirection failed during local login: {}", e.getMessage());
-//            throw new AuthException(ErrorStatus.REDIRECTION_FAILED);
         } catch (AuthException ex) {
             throw new BadRequestException(ErrorStatus.USER_NOT_FOUND);
         } catch (BadCredentialsException ex) {
