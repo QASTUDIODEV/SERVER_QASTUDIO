ddfeb205213833c0ae8ff173990f19de7a4c1091
techncherry <technarchitect@gmail.com>
2025-01-30 23:45:30 +0900

    ✨ Feature: 프로젝트 별 역할 list 조회 예외처리 추가

--- src/main/java/qastudio/backend/domain/project/service/CharacterQueryServiceImpl.java
+++ src/main/java/qastudio/backend/domain/project/service/CharacterQueryServiceImpl.java
@@ -6,11 +6,15 @@ import org.springframework.transaction.annotation.Transactional;
 import qastudio.backend.domain.project.converter.CharacterConverter;
 import qastudio.backend.domain.project.dto.response.CharacterResponse;
 import qastudio.backend.domain.project.entity.Page;
+import qastudio.backend.domain.project.entity.Project;
 import qastudio.backend.domain.project.repository.Page.PageRepository;
 import qastudio.backend.domain.project.entity.CharacterTable;
 
 import java.util.List;
 import qastudio.backend.domain.project.repository.CharacterTableRepository.CharacterTableRepository;
+import qastudio.backend.domain.project.repository.Project.ProjectRepository;
+import qastudio.backend.global.apiPayload.code.exception.custom.BadRequestException;
+import qastudio.backend.global.apiPayload.code.status.ErrorStatus;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +24,13 @@ public class CharacterQueryServiceImpl implements CharacterQueryService{
     private final CharacterTableRepository characterRepository;
     private final CharacterConverter characterConverter;
     private final PageRepository pageRepository;
+    private final ProjectRepository projectRepository;
 
     @Override
     public CharacterResponse.DetailCharacterList getDetailCharacterList(Long projectId) {
+        Project project = projectRepository.findByProjectId(projectId)
+                .orElseThrow(() -> new BadRequestException(ErrorStatus.PROJECT_NOT_FOUND));
+
         List<CharacterTable> characterTables = characterRepository.findAllByProjectId(projectId); // N+1 문제 해결 필요
         return characterConverter.toDetailCharacterList(characterTables);
     }
