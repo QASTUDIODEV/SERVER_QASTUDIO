914111917ec1848431da7335b4b26ac5e74a353e
techncherry <technarchitect@gmail.com>
2025-02-02 14:25:23 +0900

    🔨 Refactor: 불필요한 import 삭제

--- src/main/java/qastudio/backend/domain/project/service/CharacterCommandServiceImpl.java
+++ src/main/java/qastudio/backend/domain/project/service/CharacterCommandServiceImpl.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import jakarta.persistence.EntityNotFoundException;
 
 import java.time.Duration;
 import java.util.Collections;
--- src/main/java/qastudio/backend/domain/scenario/service/ScenarioCommandServiceImpl.java
+++ src/main/java/qastudio/backend/domain/scenario/service/ScenarioCommandServiceImpl.java
@@ -1,12 +1,8 @@
-
 package qastudio.backend.domain.scenario.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,21 +11,13 @@ import qastudio.backend.domain.project.entity.Page;
 import qastudio.backend.domain.project.repository.CharacterTableRepository.CharacterTableRepository;
 import qastudio.backend.domain.project.repository.Page.PageRepository;
 import org.springframework.transaction.annotation.Transactional;
-import qastudio.backend.domain.scenario.dto.request.ActionRequest;
 import qastudio.backend.domain.scenario.dto.request.ScenarioRequest;
 import qastudio.backend.domain.scenario.dto.response.ScenarioResponse;
-import qastudio.backend.domain.scenario.entity.ActionTable;
-import qastudio.backend.domain.scenario.entity.Feature;
 import qastudio.backend.domain.scenario.entity.Scenario;
-import qastudio.backend.domain.scenario.repository.ActionTableRepository;
-import qastudio.backend.domain.scenario.repository.FeatureRepository;
 import qastudio.backend.domain.scenario.repository.ScenarioRepository;
-import qastudio.backend.domain.scenario.service.ScenarioCommandService;
-import org.springframework.transaction.annotation.Transactional;
 import qastudio.backend.domain.user.entity.User;
 import qastudio.backend.domain.user.repository.User.UserRepository;
 
-import qastudio.backend.domain.user.entity.User;
 import qastudio.backend.global.apiPayload.code.exception.custom.BadRequestException;
 import qastudio.backend.global.apiPayload.code.status.ErrorStatus;
 
