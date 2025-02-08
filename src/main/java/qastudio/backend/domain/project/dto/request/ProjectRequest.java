7b31f9a57ea8b9f3d788b60e9c0ce34a3a2ff2a0
techncherry <technarchitect@gmail.com>
2025-02-09 08:36:05 +0900

    🔨 Refactor: 불필요한 import 제거

--- src/main/java/qastudio/backend/domain/project/dto/request/ProjectRequest.java
+++ src/main/java/qastudio/backend/domain/project/dto/request/ProjectRequest.java
@@ -2,12 +2,9 @@ package qastudio.backend.domain.project.dto.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Getter;
-import qastudio.backend.domain.project.entity.enums.ViewType;
 
 public class ProjectRequest {
 
